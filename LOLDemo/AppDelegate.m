4802a61e67c4ccb56ced8d5858905f4a7c7aa1ac
happying <yufuld@gmail.com>
2015-03-19 22:44:12 +0800

    删除无用的类

--- LOLDemo/AppDelegate.m
+++ LOLDemo/AppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
 #import "MySelfDataForLOLDemoViewController.h"
 #import "NewsAndImagePlayerViewController.h"
 #import "TopTabViewController.h"
